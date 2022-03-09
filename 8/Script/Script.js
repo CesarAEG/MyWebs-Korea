fc9b1cd5f4212cc8dee289762b0b3b4338846aaa
César Esquivel <99063516+CesarAEG@users.noreply.github.com>
2022-03-08 21:23:51 -0600

    Add files via upload

---
+++ 8/Script/Script.js
@@ -0,0 +1,28 @@
+$(function(){
+    var navBg = $(".nav-bg");
+    
+    $(".main-nav > ul > li").mouseover(function(){
+        $(this).find(".sub-nav").stop().slideDown(250);
+        navBg.stop(true).slideDown(250);
+    }).mouseout(function(){
+        $(this).find(".sub-nav").stop().slideUp(250);
+        navBg.stop(true).slideUp(250);
+    });
+
+    function slideShow (){
+        $(".slides").animate({
+            left : -100 + "%"
+        }, 500, function(){
+            $(this).css({ left : 0 });
+            $(this).append( $(this).find(">div").eq(0) );
+        });
+    }
+    setInterval(slideShow, 3000);
+
+    $(".notice ul li:first-child a").on("click", function(){
+        $("#modal").stop(true).fadeIn(250);
+    });
+    $("button.cls").on("click", function(){
+        $("#modal").stop(true).fadeOut(250);
+    });
+});
